a5a516c8c9445aef6dd7bf7cbd7ab171c225dfea
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-10-15 17:51:37 -0400

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         AdBlocker PopUp Block
+// @version      0.1
+// @description  None
+// @author       Phillip
+// @match        https://youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const Element = 'style-scope ytd-popup-container';
+    const Remove = document.getElementById(Element);
+    if (Remove) { Remove.remove(); }
+})();
